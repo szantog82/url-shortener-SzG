60fa465fca1d534d17ab40c81d641ac5608b2c88
Gábor Szántó <szantog82@gmail.com>
2016-09-27 12:36:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var url = require("url");
 var app = express();
 var fs = require("fs");
 var output; //if output happens, JSON output will be in this variable
-var uri = 'mongodb://' + process.env.USER + ':' + process.env.PASS + '@' +
-    process.env.HOST + ':' + process.env.MONGOPORT + '/' + process.env.DB;
+var uri = 'mongodb://mongodb:fcc007@ds033966.mlab.com:33966/szantog82';
 var entryid;
 var entrypage;
 var shortened = "https://urlshortener-szg.herokuapp.com/";
