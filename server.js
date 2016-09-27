fae066fe327b528cd0d83dbc97229a3c3da32d80
Gábor Szántó <szantog82@gmail.com>
2016-09-27 09:16:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,47 @@ var mongodb = require('mongodb');
 var http = require("http");
 var url = require("url");
 var app = express();
+var fs = require("fs");
 
 
 var server = http.createServer(function(req, res) {
-var currpath = JSON.stringify(url.parse(request.url).path);
-var input = decodeURIComponent(currpath.slice(2,currpath.length-1));
-res.writeHeader(200, {"Content-Type": "text/html"}); 
+    var currpath = JSON.stringify(url.parse(req.url).path);
+    var input = decodeURIComponent(currpath.slice(2,currpath.length-1));
+    if (currpath.length < 4) {
+    fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end();
+  })
+}
+ else if (/[0-9]/.test(input)){   
+    res.writeHeader(200, {"Content-Type": "text/html"}); 
   
-  res.write(index.html)
-  res.end()
+    res.write("number was searched")
+    res.end()
+  }
+  else if (/^new\/http:\/\//.test(input)){
+    res.write("new entry was seacrhed")
+    res.end()
+  }
+  else {
+    //res.write("Bad format or not existing id. Try again!")
+     res.write(input);
+    res.end()
+  }
 })
 
 
-server.listen(process.env.PORTT || 8888)
+server.listen(process.env.PORT || 8080)
 
 
 
 
 
-/*var uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.PORT+'/'+process.env.DB;
+/*var uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.MONGOPORT+'/'+process.env.DB;
 
 mongodb.MongoClient.connect(uri, function(err, db) {
   if(err) throw err;
